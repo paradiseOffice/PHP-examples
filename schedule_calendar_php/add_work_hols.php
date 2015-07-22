bc7da4681a46fcb96145a04d7084307864ab126b
Hazel Windle <lead-dev@linux-paradise.co.uk>
2015-07-22 08:44:28 +0000

    Fixed PDO mysql driver on Gentoo web server (recompiled PHP).

--- schedule_calendar_php/add_work_hols.php
+++ schedule_calendar_php/add_work_hols.php
@@ -60,16 +60,8 @@
      <title>Tempus - New Event</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="" content="" />
-    <link rel="stylesheet" type="text/css" href="scripts/lib/bootstrap-3.1.1-dist/css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/fonts.css" />
-    <link rel="stylesheet" type="text/css" href="css/main.css" />
-<script type="text/JavaScript"
-src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js">
-</script>
-<script type="text/JavaScript" src="scripts/lib/jquery-1.11.2-min.js"></script>
-<script type="text/JavaScript" src="scripts/lib/jquery-ui.min.js"></script>
-<script type="text/JavaScript" src="scripts/lib/bootstrap-3.1.1-dist/js/bootstrap.min.js"></script>
-<script type="text/JavaScript" src="scripts/application.js"></script>
+    <?php include_once("links.php"); ?>
+   
 </head>
 <body>
 
